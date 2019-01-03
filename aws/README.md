4e0f0bed8e3f95847dd1c850b4fe8cb33ba76e87
utkarsh867 <utkarsh867@gmail.com>
2019-01-04 00:29:58 +0530

    Python Lambda function

--- aws/README.md
+++ aws/README.md
@@ -1,5 +1,9 @@
-# AWS Services
-* AWS Lambda 
-* Amazon Lex
-* Amazon Polly
-* Amazon Transcribe
+# AWS Lambda function
+The function accepts data in the format:
+```
+{
+    "text": "<text>"
+}
+```
+
+Returns: pre-signed URL for the mp3 file.
---
+++ aws/lambda_function.py
@@ -0,0 +1,39 @@
+import json
+import os
+import base64
+import boto3
+from contextlib import closing
+
+
+def lambda_handler(event, context):
+    print(event)
+    givenText = json.loads(event['body'])
+    givenText = givenText['text']
+    print(givenText)
+    client = boto3.client('polly')
+    data = client.synthesize_speech(OutputFormat='mp3', Text= givenText, VoiceId='Joanna')
+    postId = "file"
+    if "AudioStream" in data:
+        with closing(data["AudioStream"]) as stream:
+            output = os.path.join("/tmp/", postId)
+            with open(output, "ab") as file:
+                file.write(stream.read())
+
+    s3 = boto3.client('s3')
+    s3.upload_file('/tmp/' + postId, 
+      os.environ['BUCKET'], 
+      postId + ".mp3")
+      
+    preSignedUrl = s3.generate_presigned_url(ClientMethod='get_object',Params={'Bucket': os.environ['BUCKET'],'Key': postId + ".mp3"})
+    
+    response = {
+        'statusCode': 200,
+        'headers': {
+            'content-type': 'application/json', 
+            'Access-Control-Allow-Origin': "*",
+        },
+        'body': preSignedUrl,
+        'isBase64Encoded': True
+    }
+
+    return response
