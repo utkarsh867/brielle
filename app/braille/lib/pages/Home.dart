d40f7bae95db403552a556a4647ad9bfd0d452fa
“ashutosh <160020020@iitdh.ac.in>
2019-01-05 02:09:24 +0530

    added audio

--- app/braille/lib/pages/Home.dart
+++ app/braille/lib/pages/Home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
 
 import 'braille_letter.dart';
 
@@ -7,7 +10,6 @@ class HomePopupMenu {
   final String title;
   final IconData icon;
 }
-
 
 const List<HomePopupMenu> menuItems = const <HomePopupMenu>[
   const HomePopupMenu(title: 'MenuItem1', icon: Icons.home)
@@ -21,49 +23,152 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-void letterlogic(String letter, BuildContext context) {
+void letterlogic(String letter, BuildContext context,String url) {
   List<bool> vibbutton;
-  letter=letter.toUpperCase();
-  switch(letter){
-    case "A": {vibbutton = [true, false, false, false, false, false];break;}
-    case "B": {vibbutton = [true, false, true, false, false, false];break;}
-    case "C": {vibbutton = [true, true, false, false, false, false];break;}
-    case "D": {vibbutton = [true, true, false, true, false, false];break;}
-    case "E": {vibbutton = [true, false, false, true, false, false];break;}
-    case "F": {vibbutton = [true, true, true, false, false, false];break;}
-    case "G": {vibbutton = [true, true, true, true, false, false];break;}
-    case "H": {vibbutton = [true, false, true, true, false, false];break;}
-    case "I": {vibbutton = [false,true, true, false, false, false];break;}
-    case "J": {vibbutton = [false , true, true, true, false, false];break;}
-    case "K": {vibbutton = [true, false, false, false, true, false];break;}
-    case "L": {vibbutton = [true, false, true, false, true, false];break;}
-    case "M": {vibbutton = [true, true, false, false, true, false];break;}
-    case "N": {vibbutton = [true, true, false, true, true, false];break;}
-    case "O": {vibbutton = [true, false, false, true, true, false];break;}
-    case "P": {vibbutton = [true, true, true, false, true, false];break;}
-    case "Q": {vibbutton = [true, true, true, true, true, false];break;}
-    case "R": {vibbutton = [true, false, true,true, true, false];break;}
-    case "S": {vibbutton = [false, true, true, false, true, false];break;}
-    case "T": {vibbutton = [false, true, true, true, true, false];break;}
-    case "U": {vibbutton = [true, false, false, false, true, true];break;}
-    case "V": {vibbutton = [true, false, true, false, true, true];break;}
-    case "W": {vibbutton = [false, true, true, true, false, true];break;}
-    case "X": {vibbutton = [true, true, false, false, true, true];break;}
-    case "Y": {vibbutton = [true, true, false, true, true, true];break;}
-    case "Z": {vibbutton = [true, false, false, true, true, true];break;}
-
+  letter = letter.toUpperCase();
+  switch (letter) {
+    case "A":
+      {
+        vibbutton = [true, false, false, false, false, false];
+        break;
+      }
+    case "B":
+      {
+        vibbutton = [true, false, true, false, false, false];
+        break;
+      }
+    case "C":
+      {
+        vibbutton = [true, true, false, false, false, false];
+        break;
+      }
+    case "D":
+      {
+        vibbutton = [true, true, false, true, false, false];
+        break;
+      }
+    case "E":
+      {
+        vibbutton = [true, false, false, true, false, false];
+        break;
+      }
+    case "F":
+      {
+        vibbutton = [true, true, true, false, false, false];
+        break;
+      }
+    case "G":
+      {
+        vibbutton = [true, true, true, true, false, false];
+        break;
+      }
+    case "H":
+      {
+        vibbutton = [true, false, true, true, false, false];
+        break;
+      }
+    case "I":
+      {
+        vibbutton = [false, true, true, false, false, false];
+        break;
+      }
+    case "J":
+      {
+        vibbutton = [false, true, true, true, false, false];
+        break;
+      }
+    case "K":
+      {
+        vibbutton = [true, false, false, false, true, false];
+        break;
+      }
+    case "L":
+      {
+        vibbutton = [true, false, true, false, true, false];
+        break;
+      }
+    case "M":
+      {
+        vibbutton = [true, true, false, false, true, false];
+        break;
+      }
+    case "N":
+      {
+        vibbutton = [true, true, false, true, true, false];
+        break;
+      }
+    case "O":
+      {
+        vibbutton = [true, false, false, true, true, false];
+        break;
+      }
+    case "P":
+      {
+        vibbutton = [true, true, true, false, true, false];
+        break;
+      }
+    case "Q":
+      {
+        vibbutton = [true, true, true, true, true, false];
+        break;
+      }
+    case "R":
+      {
+        vibbutton = [true, false, true, true, true, false];
+        break;
+      }
+    case "S":
+      {
+        vibbutton = [false, true, true, false, true, false];
+        break;
+      }
+    case "T":
+      {
+        vibbutton = [false, true, true, true, true, false];
+        break;
+      }
+    case "U":
+      {
+        vibbutton = [true, false, false, false, true, true];
+        break;
+      }
+    case "V":
+      {
+        vibbutton = [true, false, true, false, true, true];
+        break;
+      }
+    case "W":
+      {
+        vibbutton = [false, true, true, true, false, true];
+        break;
+      }
+    case "X":
+      {
+        vibbutton = [true, true, false, false, true, true];
+        break;
+      }
+    case "Y":
+      {
+        vibbutton = [true, true, false, true, true, true];
+        break;
+      }
+    case "Z":
+      {
+        vibbutton = [true, false, false, true, true, true];
+        break;
+      }
   }
-  
+
   Navigator.push(
     context,
     MaterialPageRoute(
-      builder: (BuildContext context) => BrailleLetter(letter, vibbutton),
+      builder: (BuildContext context) => BrailleLetter(letter, vibbutton,url),
     ),
   );
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String _letter = '';
+  String _letter = "";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -105,7 +210,24 @@ class _MyHomePageState extends State<MyHomePage> {
               child: Text('Learn'),
               color: Theme.of(context).accentColor,
               textColor: Colors.white,
-              onPressed: () => letterlogic(_letter, context),
+              onPressed: () {
+                //letterlogic(_letter, context);
+               //print('Hello1'+_letter);
+                final Map<String, String> data = {
+                   "text": _letter
+                  };
+                  print(json.encode(data));
+                http.post(
+                    'https://aj6yfs2f3b.execute-api.us-east-1.amazonaws.com/deploy/braille_python',
+                    headers: {'x-api-key':"F11Z6OB29PjLVp4lVb6o9aKQRUlfvRC1yc6GczSd"},
+                    body: json.encode(data)).then(( http.Response response) {
+                     // final Map<String,dynamic> responseData=json.decode(response.body);   
+                     print('Hello2');
+                      print(response.body);
+                      letterlogic(_letter, context,response.body);
+                    });
+                
+              },
             )
           ],
         ),
--- app/braille/lib/pages/braille_letter.dart
+++ app/braille/lib/pages/braille_letter.dart
@@ -1,12 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayer/audioplayer.dart';
 
 import '../vibrate_button.dart';
+class SoundManager {
+  AudioPlayer audioPlayer = new AudioPlayer();
+  Future playLocal(url) async {   
+    await audioPlayer.play(url);
+  }
+}
 
-class BrailleLetter extends StatelessWidget {
+class BrailleLetter extends StatefulWidget {
   final List<bool> vibbutton;
   final String letter;
+  final String audioUrl;
+  BrailleLetter(this.letter, this.vibbutton,this.audioUrl);
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _BrailleLetter();
+  }
 
-  BrailleLetter(this.letter, this.vibbutton);
+  void onLoad(BuildContext context) {
+
+    SoundManager soundManager = new SoundManager();
+
+    soundManager.playLocal(audioUrl);
+  }
+}
+
+class _BrailleLetter extends State<BrailleLetter> {
+  
+  void initState() {
+    super.initState();
+    widget.onLoad(context);
+  } 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -21,7 +48,7 @@ class BrailleLetter extends StatelessWidget {
           children: <Widget>[
             Container(
               child: Text(
-                letter,
+                widget.letter,
                 style: TextStyle(fontWeight: FontWeight.bold, fontSize: 100.0),
               ),
             ),
@@ -34,17 +61,17 @@ class BrailleLetter extends StatelessWidget {
                   Column(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: <Widget>[
-                      VibrateButton(vibbutton[0]),
-                      VibrateButton(vibbutton[2]),
-                      VibrateButton(vibbutton[4])
+                      VibrateButton(widget.vibbutton[0]),
+                      VibrateButton(widget.vibbutton[2]),
+                      VibrateButton(widget.vibbutton[4])
                     ],
                   ),
                   Column(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: <Widget>[
-                      VibrateButton(vibbutton[1]),
-                      VibrateButton(vibbutton[3]),
-                      VibrateButton(vibbutton[5])
+                      VibrateButton(widget.vibbutton[1]),
+                      VibrateButton(widget.vibbutton[3]),
+                      VibrateButton(widget.vibbutton[5])
                     ],
                   )
                 ],
